9843167d24e4bb4932dee2154f3bfce4c8f9d542
梁策 <dxlc2014@hotmail.com>
2022-11-11 17:30:55 +0800

    feat: 添加standardversion

--- changelog-option.js
+++ changelog-option.js
@@ -36,45 +36,45 @@ module.exports = {
       }
 
 
-      // if (commit.scope === '*') {
-      //   commit.scope = ''
-      // }
-      // if (typeof commit.hash === 'string') {
-      //   commit.hash = commit.hash.substring(0, 7)
+      if (commit.scope === '*') {
+        commit.scope = ''
+      }
+      if (typeof commit.hash === 'string') {
+        commit.hash = commit.hash.substring(0, 7)
 
-      // }
-      // if (typeof commit.subject === 'string') {
-      //   let url = context.repository
-      //     ? `${context.host}/${context.owner}/${context.repository}`
-      //     : context.repoUrl
-      //   if (url) {
-      //     url = `${url}/issues/`
-      //     // Issue URLs.
-      //     commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
-      //       issues.push(issue)
-      //       return `[#${issue}](${url}${issue})`
-      //     })
-      //   }
-      //   if (context.host) {
-      //     // User URLs.
-      //     commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
-      //       if (username.includes('/')) {
-      //         return `@${username}`
-      //       }
+      }
+      if (typeof commit.subject === 'string') {
+        let url = context.repository
+          ? `${context.host}/${context.owner}/${context.repository}`
+          : context.repoUrl
+        if (url) {
+          url = `${url}/issues/`
+          // Issue URLs.
+          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+            issues.push(issue)
+            return `[#${issue}](${url}${issue})`
+          })
+        }
+        if (context.host) {
+          // User URLs.
+          commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
+            if (username.includes('/')) {
+              return `@${username}`
+            }
 
-      //       return `[@${username}](${context.host}/${username})`
-      //     })
-      //   }
-      // }
+            return `[@${username}](${context.host}/${username})`
+          })
+        }
+      }
 
-      // // remove references that already appear in the subject
-      // commit.references = commit.references.filter(reference => {
-      //   if (issues.indexOf(reference.issue) === -1) {
-      //     return true
-      //   }
+      // remove references that already appear in the subject
+      commit.references = commit.references.filter(reference => {
+        if (issues.indexOf(reference.issue) === -1) {
+          return true
+        }
 
-      //   return false
-      // })
+        return false
+      })
       return commit
     },
     groupBy: 'type',
