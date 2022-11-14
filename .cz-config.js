bf9daf23c33a0e3ff4c6e0b2d0aee322a3cbf211
梁策 <dxlc2014@hotmail.com>
2022-11-14 16:46:24 +0800

    fix: 修改了一个大BUG

--- .cz-config.js
+++ .cz-config.js
@@ -13,19 +13,20 @@ module.exports = {
     { value: "build", name: "👷‍ Build System | 构建" },
     { value: "ci", name: "🔧 Continuous Integration | CI 配置" },
   ],
-  scopes: [{ name: '' }],
+  scopes: [{ name: "" }],
   // 消息步骤
   messages: {
-    type: "请选择提交类型:",
-    subject: "请简要描述提交(必填):",
-    body: "请输入详细描述(可选):",
-    footer: "请输入要关闭的issue(可选):",
+    type: "请选择提交类型：",
+    subject: "请简要描述提交(必填)：",
+    body: "请输入详细描述(可选)：",
+    footer: "请输入要关闭的issue(可选)：",
     confirmCommit: "确认使用以上信息提交？(y/n/e/h)",
+    breaking: "列出破坏性更改（不兼容）（可选）：",
   },
   // 跳过问题
-  skipQuestions: ["scope","body", "footer"],
+  skipQuestions: ["scope", "body", "footer"],
   allowCustomScopes: false,
   // subject文字长度默认是72
   subjectLimit: 72,
-  allowBreakingChanges: ["feat", "fix"]
+  allowBreakingChanges: ["feat", "fix"],
 };
